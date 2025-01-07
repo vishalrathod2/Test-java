bafa37e901e7381fc63d518ebf123a7862c2fa9c
vishalrathod2 <rathodvishal98131@gmail.com>
2025-01-07 23:31:28 +0530

    value enterd by user and storing in costructor

--- Java Learning/Test1.java
+++ Java Learning/Test1.java
@@ -1,19 +1,41 @@
+import java.util.Scanner;
 class A 
-{   void show()
+{   
+    Scanner sc = new Scanner(System.in);
+    int a ,b ;
+    void input()
     {
-    System.out.println("Hello Vishal");
+        System.out.println("Enter the value of a");
+        a = sc.nextInt();
+        System.out.println("Enter the value of b");
+        b = sc.nextInt();
     }
+    A()
+    {
+        int a;
+        // System.out.println(); //constructor
+    }
+// {   void show()
+//     {
+//     System.out.println("Hello Vishal");
+//     }
     // int a = 200; //instance variable
 }
 class B extends A 
-{
-    // int a = 100; //instance variable
-    void show()
-    {    
-        super.show();
-        System.out.println("Hello Virat Bhai");
-    //     System.out.println(super.a); //super keyword is used to refer immediate parent class instance variable
+{ 
+
+    B()
+    {
+        int b;
+        // System.out.println(); //constructor
     }
+    // int a = 100; //instance variable
+    // void show()
+    // {    
+    //     super.show();
+    //     System.out.println("Hello Virat Bhai");
+    // //     System.out.println(super.a); //super keyword is used to refer immediate parent class instance variable
+    // }
     // }
 }
 
@@ -22,7 +44,10 @@ class Test1
     public static void main(String[] args) 
     {
         B r = new B();
-        r.show();
+        r.input();
+        System.out.println("The value of a is:"+r.a);
+        System.out.println("The value of b is:"+r.b);
+        // r.show(); //method calling
 
     }
 }
