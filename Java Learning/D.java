17a0013cf81ec09157b4d9c150e209041eefcd6b
vishalrathod2 <rathodvishal98131@gmail.com>
2025-01-08 08:16:39 +0530

    this keyword

---
+++ Java Learning/D.java
@@ -0,0 +1,31 @@
+// class D
+// {
+//    void show()
+//    {
+//          System.out.println(this);
+//     }
+
+//    // public static void main(String[] args)
+//     {
+//         D d = new D();
+//         d.show();
+//         System.out.println(d);
+//     }
+// }
+// class D 
+// {   int d;
+//     D(int d)
+//     {
+//       this. d = d;
+//     }
+//     void show()
+//     {
+//         System.out.println(d);
+//     }
+// //    public static void main(String[] args)
+//     {
+//         D d = new D(10);
+//         d.show();
+      
+//     }
+// }
