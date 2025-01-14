7e57a1c4939754b7a6d7fed5c36a5475c33e1246
vishalrathod2 <rathodvishal98131@gmail.com>
2025-01-14 20:54:17 +0530

    Simple gui form into add new field

--- Java Learning/SimpleGUI.java
+++ Java Learning/SimpleGUI.java
@@ -11,18 +11,26 @@ public class SimpleGUI {
         frame.setLayout(null);
 
         // Create a label
-        JLabel label = new JLabel("Enter your name:");
+        JLabel label = new JLabel("Enter your First Name:");
         label.setBounds(50, 30, 120, 25);
         frame.add(label);
+
+        JLabel label2 = new JLabel("Enter your Last Name");
+        label2.setBounds(50,70,120,25);
+        frame.add(label2);
 
         // Create a text field
         JTextField textField = new JTextField();
         textField.setBounds(180, 30, 150, 25);
         frame.add(textField);
 
+        JTextField textField2 = new JTextField();
+        textField2.setBounds(180, 70, 150, 25); 
+        frame.add(textField2);
+
         // Create a button
         JButton button = new JButton("Click Me");
-        button.setBounds(50, 80, 100, 30);
+        button.setBounds(70, 100, 100, 30);
         frame.add(button);
 
         // Create a label to display the message
@@ -35,12 +43,18 @@ public class SimpleGUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String name = textField.getText();
-                if (!name.isEmpty()) {
-                    messageLabel.setText("Hello, " + name + "!");
+                String name2 = textField2.getText();
+                if (!name.isEmpty() && !name2.isEmpty()) {
+                    messageLabel.setText("First Name: " + name + ", Last Name: " + name2);
+                } else if (name.isEmpty()) {
+                    messageLabel.setText("Please enter your First Name.");
+                } else if (name2.isEmpty()) {
+                    messageLabel.setText("Please enter your Last Name.");
                 } else {
-                    messageLabel.setText("Please enter your name.");
+                    messageLabel.setText("Please enter both First and Last Names.");
                 }
             }
+            
         });
 
         // Make the frame visible
